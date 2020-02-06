9442c5c4995e5afc81f5c2e5374e20890b7c6e21
fesasabil <kijee212@gmail.com>
2020-02-06 22:50:16 +0700

    add_map

---
+++ src/firstGo/map.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var chicken map[string]int
+	chicken = map[string]int{}
+
+	chicken["januari"] = 50
+	chicken["februari"] = 40
+
+	fmt.Println("januari", chicken["januari"])
+	fmt.Println("mei", chicken["mei"])
+
+	// // Inisialisasi Nilai Map
+	// var data map[string]int
+	// data["one"] = 1
+
+	// data = map[string]int{}
+	// data["one"] = 1
+
+	// // cara vertikal
+	// var chicken1 = map[string]int{"januari": 50, "februari": 40}
+
+	// // cara horizontal
+	// var chicken2 = map[string]int{
+	// 	"januari": 50,
+	// 	"februari": 40,
+	// }
+
+	// var chicken3 = map[string]int{}
+	// var chicken4 = make(map[string]int)
+	// var chicken5 = *new(map[string]int)
+
+	// Iterasi Item Map Menggunakan for - range
+	var chickens = map[string]int{
+		"januari": 50,
+		"februari": 40,
+		"maret": 34,
+		"april": 67,
+	}
+
+	for key, val := range chickens {
+		fmt.Println(key, " \t:", val)
+	}
+
+	// Menghapus Item Map
+	var chicken3 = map[string]int{"januari": 50, "februari": 40}
+
+	fmt.Println(len(chicken3))
+	fmt.Println(chicken3)
+
+	delete(chicken3, "januari")
+
+	fmt.Println(len(chicken3))
+	fmt.Println(chicken3)
+
+	// Deteksi Keberadaan Item Dengan Key Tertentu
+	var chicken4 = map[string]int{"januari": 50, "februari": 40}
+	var value, isExist = chicken4["mei"]
+
+	if isExist {
+		fmt.Println(value)
+	} else {
+		fmt.Println("item is not exists")
+	}
+
+	// 
+}
